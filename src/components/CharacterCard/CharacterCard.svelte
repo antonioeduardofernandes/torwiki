9fb305272056afd74bf8cf69fcde3df4b7d9bb1b
antonioeduardofernandes <antonioeduardofernandes@gmail.com>
2022-04-14 11:10:11 -0300

    bottom description on character card merged to descriptions

--- src/components/CharacterCard/CharacterCard.svelte
+++ src/components/CharacterCard/CharacterCard.svelte
@@ -1,15 +1,13 @@
 <script>
     import TopContent from "./TopContent.svelte";
     import Descriptions from "./Descriptions.svelte";
-    import BottomContent from "./BottomContent.svelte";
     import Illustrations from "./Illustrations.svelte";
 </script>
 
 <div class="container">
     <TopContent />
     <Descriptions />
-    <BottomContent />
-    <Illustrations />  
+    <Illustrations />
 </div>
 
 <style>
@@ -17,12 +15,4 @@
         display: flex;
         flex-flow: column;
     }
-    /* section {
-        display: flex;
-        flex-flow: column;
-        height: 100vh;
-        position: relative;
-    } */
-
-    
 </style>
